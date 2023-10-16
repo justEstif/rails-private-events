5a6a7b18d549e7d19ec1679c87313e84dc586efa
Estifanos <95101485+justEstif@users.noreply.github.com>
2023-10-16 06:26:29 -0400

    feat: create scopes for past and upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
+  scope :past, -> { where("date < ?", Time.now) }
+  scope :upcoming, -> { where("date >= ?", Time.now) }
+
   belongs_to :creator, class_name: "User"
   has_many :tickets
   has_many :attendees, through: :tickets, source: :attendee
